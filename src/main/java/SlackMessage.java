91e903b9d25dcb774c6019871fd06d23260f3e68
Wolfgang Hauptfleisch <dev@augmentedlogic.com>
2020-08-07 14:48:54 +0100

    typo fix

--- src/main/java/SlackMessage.java
+++ src/main/java/SlackMessage.java
@@ -1,5 +1,5 @@
 /**
-  Copyright (c) 2019 Wolfgang Hauptfleisch <wh@augmentedlogic.com>
+  Copyright (c) 2019 Wolfgang Hauptfleisch <dev@augmentedlogic.com>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
